8fda525f725d7ef2cbb957d8a9f6fc4088864332
Hemin Shah <hemin.shah@server.com-new>
2025-09-09 10:09:51 +0530

    login and admin url path redirect changes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    if (\Auth::guard('admin')->check()) {
+        return redirect()->route('admin.admin.listing');
+    }
+    return redirect()->route('admin.login');
+});
+
+Route::fallback(function () {
+    if (\Auth::guard('admin')->check()) {
+        return redirect()->route('admin.admin.listing');
+    }
+    return redirect()->route('admin.login');
 });
